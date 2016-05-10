709709fdf2a903eadd39eee9b10474999ec8de74
Julio Coimbra <julio.coimbra@gmail.com>
2016-05-10 16:10:06 -0400

    Implementado lista de itens no modulo Materiais

--- estoque/views.py
+++ estoque/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from estoque.models import Estoque, Local
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from estoque.models import Estoque, Local, Categoria
 
 def index(request):
-	return render(request, 'estoque_index.html', { 'estoques' : Estoque.objects.all() } )
+    estoque_list = Estoque.objects.all()
+    paginator = Paginator(estoque_list, 12) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        estoques = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        estoques = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        estoques = paginator.page(paginator.num_pages)
+
+    return render(request, 'estoque_index.html', { 'estoques': estoques })
